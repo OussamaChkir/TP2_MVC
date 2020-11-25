403721e77c0424ffa9451cc0707b53de6cb4af6c
OussamaChkir <oussama501chkir@gmail.com>
2020-11-25 17:46:38 +0100

    init

---
+++ MVC_TP2/Controllers/EmployeeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_TP2.Models;
+using MVC_TP2.Models.Repositories;
+
+namespace MVC_TP2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            return View(model);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = _employeeRepository.GetEmployee(id);
+            return View(model);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee e = _employeeRepository.Add(emp);
+                if (e != null)
+                    return RedirectToAction(nameof(Index));
+                else
+                    return View();
+            }
+            return View();
+
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee emp)
+        {
+            Employee e = _employeeRepository.Update(emp);
+
+                return View();
+            
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC_TP2/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_TP2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required][MaxLength(50,ErrorMessage ="Taille Max 50 cc")]
+        public string Name { get; set; }
+        public string Departement { get; set; }
+        [Range(300,5000,ErrorMessage ="Doit etre entre 300 et 5000")]
+        public int salary { get; set; }
+    }
+}
